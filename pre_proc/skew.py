3ffd6883e91ec6509ecf23a8fff4fa19bf7a2ca0
rushyam <rushyamsonu@gmail.com>
2018-03-24 12:22:43 +0530

    added skew correction

---
+++ pre_proc/skew.py
@@ -0,0 +1,22 @@
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+ 
+image = cv2.imread("tilt.png")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.bitwise_not(gray)
+thresh = cv2.threshold(gray, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+coords = np.column_stack(np.where(thresh > 0))
+angle = cv2.minAreaRect(coords)[-1]
+if angle < -45:
+	angle = -(90 + angle)
+else:
+	angle = -angle
+(h, w) = image.shape[:2]
+center = (w // 2, h // 2)
+M = cv2.getRotationMatrix2D(center, angle, 1.0)
+rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+cv2.imshow("Input", image)
+cv2.imshow("Rotated", rotated)
+cv2.waitKey(0)
