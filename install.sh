dc54229a3194276ef01e7368be20f7a1e964946f
khadok <satyanandiam@gmail.com>
2018-03-29 18:49:44 +0530

    install test

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # install dependencies
-sudo -E apt-get install pip
+sudo apt-get install software-properties-common
+sudo apt-add-repository universe
+sudo apt-get update
+sudo -E apt-get install python-pip
 sudo -E apt-get install python-qt4
 sudo -E apt-get python install scipy
-sudo -E apt-get update
